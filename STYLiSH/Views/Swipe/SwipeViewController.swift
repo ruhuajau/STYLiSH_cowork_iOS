69653661d5437f25a3c0b43345669a91f885b499
Renee Hsu <piaoyunh@gmail.com>
2023-09-02 18:02:51 +0800

    Create swipe page and modal view

---
+++ STYLiSH/Views/Swipe/SwipeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SwipeViewController.swift
+//  STYLiSH
+//
+//  Created by Renee Hsu on 2023/9/2.
+//  Copyright © 2023 AppWorks School. All rights reserved.
+//
+
+import UIKit
+
+class SwipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
